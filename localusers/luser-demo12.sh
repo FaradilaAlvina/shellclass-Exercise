042b0fec936a0e2c208763e1fe76401862cfeff9
FaradilaAlvina <faradilaalvina03@gmail.com>
2021-12-16 22:04:30 +0700

    six commit

---
+++ localusers/luser-demo12.sh
@@ -0,0 +1,6 @@
+#!/bin/#!/usr/bin/env bash
+#
+# This script shows open the network ports on a system
+# Use -4 as an argument to limit to tcpv4 ports
+
+netstat -nutl ${1} | grep ':' | awk '{print $4}' | awk -F ':' '{print $NF}'
